36ba062c28dc795e892860520a3dfb1388d66060
Satyam Saxena <satyam@satyams-mbp.speedport_w_724v_typ_a_05011603_05_020>
2017-07-18 22:28:06 +0200

    flash message complete

--- client/components/app.component.js
+++ client/components/app.component.js
@@ -1,13 +1,16 @@
 import React , { Component } from 'react';
 import NavBarComponent from './nav.bar.component';
 import routes from './routes';
+import FlashMessagesComponent from './flash.messages.component';
 
 export default class App extends Component{
     render(){
         return (
             <div className='container'>
                 <NavBarComponent />
+                <FlashMessagesComponent />
                 <div className='page-header'>
+                    
                     {routes}
                 </div>
             </div>
